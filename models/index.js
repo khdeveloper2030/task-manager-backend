99de454b262fde9342c6b05bbb56f4ff074addff
khdeveloper2030 <em.seyha.hs@moeys.gov.kh>
2026-04-17 23:56:56 +0700

    Fix: explicitly provide pg module to sequelize

--- models/index.js
+++ models/index.js
@@ -9,20 +9,22 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// ១. បន្ថែមការ Require pg នៅទីនេះ
+const pg = require('pg'); 
+
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  // ការកំណត់សម្រាប់ Vercel + Neon Postgres
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres',
+    // ២. បន្ថែមបន្ទាត់នេះ ដើម្បីប្រាប់ Sequelize ឱ្យប្រើ pg ដែលយើង require ខាងលើ
+    dialectModule: pg, 
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
     },
-    // បន្ថែម Pool ដើម្បីការពារការ Crash លើ Serverless
     pool: {
       max: 5,
       min: 0,
@@ -31,31 +33,20 @@ if (process.env.DATABASE_URL) {
     },
     logging: false 
   });
-} else if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
+// ... កូដផ្នែកខាងក្រោម (fs.readdirSync...) រក្សាទុកដដែល ...
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+  if (db[modelName].associate) db[modelName].associate(db);
 });
 
 db.sequelize = sequelize;
