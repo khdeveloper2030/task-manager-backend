a9bf7b52368c1bc294b83debf7623cd632cf1dbf
khdeveloper2030 <em.seyha.hs@moeys.gov.kh>
2026-04-18 12:36:22 +0700

    Fix Error 400 and integration with Gmail Login

--- models/task.js
+++ models/task.js
@@ -1,15 +1,38 @@
-'use strict';
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-  const Task = sequelize.define('Task', {
-    title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT, allowNull: true },
-    priority: { type: DataTypes.STRING, defaultValue: 'medium' },
-    status: { type: DataTypes.STRING, defaultValue: 'todo' },
-    startDate: { type: DataTypes.DATE, allowNull: true }, // រក្សាទុកដដែល
-    endDate: { type: DataTypes.DATE, allowNull: true },   // រក្សាទុកដដែល
-    userEmail: { type: DataTypes.STRING, allowNull: false } // សម្រាប់ Auth
+  const Task = sequelize.define("Task", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    priority: {
+      type: DataTypes.STRING,
+      defaultValue: "medium",
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: "todo",
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    // កែសម្រួលត្រង់នេះ៖ ប្តូរមក true សិន ដើម្បីការពារ Error 400 ពេល Sync ជាមួយទិន្នន័យចាស់
+    userEmail: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   });
+
   return Task;
 };
